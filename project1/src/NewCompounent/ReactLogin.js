7bb16491364c389074321cd01a0f8e82516e71a5
20balamurugan <snbalamurugandev@gmail.com>
2024-06-09 12:51:15 +0530

    AddMessage

--- project1/src/NewCompounent/ReactLogin.js
+++ project1/src/NewCompounent/ReactLogin.js
@@ -53,6 +53,7 @@ const ReactLogin = () => {
   };
 
   return (
+    // Bala-Login
     <div className="bg-body-tertiary min-vh-100 d-flex flex-row align-items-center">
       <CContainer>
         <CRow className="justify-content-center">
